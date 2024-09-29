4d6083dd37f5d304883a7273810436d106d14724
unknown <pebuka351@gmail.com>
2024-09-29 22:51:14 +0100

    Build

--- src/components/application/home/faq.tsx
+++ src/components/application/home/faq.tsx
@@ -25,60 +25,68 @@ export default function FAQ() {
         Find answers to common questions about our platform and the waitlist
         process.
       </p>
-
-      <Accordion
-        type="single"
-        className="w-full large:mb-20 mb-[60px]  gap-x-[30px] gap-y-[30px] tablet:grid-cols-1 grid grid-cols-2 items-start"
-        collapsible
-      >
-        {data?.map((item: any, index: number) => {
+      <div className="w-full grid grid-cols-2 large:mb-20 tablet:grid-cols-1 items-start gap-y-[30px] gap-x-[30px] mb-[60px]">
+        {data?.map((acc: any, i: number) => {
           return (
-            <AccordionItem
-              key={index}
-              value={String(index)}
-              className="accordion-dropshadow px-8 rounded-[10px] border border-[#FFBB0A3D] bg-white"
+            <Accordion
+              type="single"
+              key={i}
+              className="col-span-1  flex flex-col gap-[30px] "
+              collapsible
             >
-              <AccordionTrigger
-                onClick={() => {
-                  if (selected !== item.question) {
-                    setSelected(item.question);
-                  } else {
-                    setSelected("");
-                  }
-                }}
-                className="items-start py-0"
-              >
-                <div className="w-full h-[100px] tablet:h-max tablet:py-6 flex gap-[30px] items-center">
-                  <div className="w-[30px] h-[30px] tablet:w-5 tablet:h-5 relative flex justify-center items-center">
-                    <div
-                      className={`w-full h-[5px] tablet:h-[3px] rounded-[20px] bg-[#14051B] duration-500  ${
-                        selected === item.question ? "opacity-0" : "opacity-100"
-                      }`}
-                    ></div>
-                    <div
-                      className={` duration-500 h-[30px] tablet:h-5 tablet:w-[3px] absolute top-0 left-[50%] w-[5px] rounded-[20px] -translate-x-[50%] ${
-                        selected === item.question
-                          ? "bg-background-darkYellow rotate-[-90deg]"
-                          : "bg-[#14051B] rotate-0"
-                      }`}
-                    ></div>
-                  </div>{" "}
-                  <h3 className="font-semibold text-[#14051B] large:text-xl text-lg text-start tablet:text-sm">
-                    {item.question}
-                  </h3>
-                </div>
-              </AccordionTrigger>
-              <AccordionContent className="w-full">
-                <div className="h-max pb-10 tablet:pb-6 w-full overflow-hidden pr-[32px] pl-[60px]">
-                  <p className="line-clamp-3 large:leading-8 leading-7 tablet:leading-5 text-[#575757] font-normal large:text-base text-[15px] tablet:text-[13px]">
-                    {item.answer}
-                  </p>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
+              {acc?.map((item: any, index: number) => {
+                return (
+                  <AccordionItem
+                    key={index}
+                    value={String(index)}
+                    className="accordion-dropshadow px-8 rounded-[10px] border border-[#FFBB0A3D] bg-white"
+                  >
+                    <AccordionTrigger
+                      onClick={() => {
+                        if (selected !== item.question) {
+                          setSelected(item.question);
+                        } else {
+                          setSelected("");
+                        }
+                      }}
+                      className="items-start py-0"
+                    >
+                      <div className="w-full h-[100px] tablet:h-max tablet:py-6 flex gap-[30px] items-center">
+                        <div className="w-[30px] h-[30px] tablet:w-5 tablet:h-5 relative flex justify-center items-center">
+                          <div
+                            className={`w-full h-[5px] tablet:h-[3px] rounded-[20px] bg-[#14051B] duration-500  ${
+                              selected === item.question
+                                ? "opacity-0"
+                                : "opacity-100"
+                            }`}
+                          ></div>
+                          <div
+                            className={` duration-500 h-[30px] tablet:h-5 tablet:w-[3px] absolute top-0 left-[50%] w-[5px] rounded-[20px] -translate-x-[50%] ${
+                              selected === item.question
+                                ? "bg-background-darkYellow rotate-[-90deg]"
+                                : "bg-[#14051B] rotate-0"
+                            }`}
+                          ></div>
+                        </div>{" "}
+                        <h3 className="font-semibold text-[#14051B] large:text-xl text-lg text-start tablet:text-sm">
+                          {item.question}
+                        </h3>
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent className="w-full">
+                      <div className="tablet:h-max h-[130px] tablet:pb-6 w-full overflow-hidden pr-[32px] pl-[60px]">
+                        <p className="line-clamp-3 large:leading-8 leading-7 tablet:leading-5 text-[#575757] font-normal large:text-base text-[15px] tablet:text-[13px]">
+                          {item.answer}
+                        </p>
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
           );
         })}
-      </Accordion>
+      </div>
 
       <div className="w-full">
         {" "}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,159 +10,141 @@ const config = {
   ],
   prefix: "",
   theme: {
-  	container: {
-  		center: 'true',
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		screens: {
-  			'380': '380px',
-  			'1186': '1186px',
-  			'2xl': '1400px',
-  			desktop: '1000px',
-  			mobile: '500px',
-  			tablet: {
-  				raw: '(max-width: 999px)'
-  			},
-  			large: {
-  				raw: '(min-width: 1440px) and (min-height: 1040px)'
-  			}
-  		},
-  		maxWidth: {
-  			desktop: '1200px',
-  			createPage: '850px'
-  		},
-  		maxHeight: {
-  			emptyBox: '250px'
-  		},
-  		width: {
-  			'107': '107px'
-  		},
-  		height: {
-  			'107': '107px',
-  			allEventsSection: 'calc(100% - 128px)'
-  		},
-  		colors: {
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: {
-  				muted: '#f9fafb',
-  				subtle: '#f3f4f6',
-  				DEFAULT: '#ffffff',
-  				emphasis: '#374151',
-  				darkYellow: '#FFBB0A',
-  				yellow700: '#EAB308',
-  				brown: '#876307',
-  				lightYellow: '#D5AE35',
-  				white300: '#FEFEFE',
-  				whisperGray: '#f8f7fa',
-  				whisperWhite: 'rgb(248, 247, 250)',
-  				darkPurple: 'rgba(30, 10, 60, 0.07)'
-  			},
-  			content: {
-  				subtle: '#9ca3af',
-  				DEFAULT: '#6b7280',
-  				emphasis: '#374151',
-  				strong: '#111827',
-  				inverted: '#ffffff',
-  				grayText: '#575757'
-  			},
-  			foreground: 'hsl(var(--foreground))',
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))',
-  				boulder: '#7B7B7B',
-  				boulder50: '#F6F6F6',
-  				boulder100: '#E7E7E7',
-  				tan: '#111111',
-  				boulder200: '#D1D1D1',
-  				boulder300: '#B0B0B0',
-  				boulder400: '#7B7B7B',
-  				boulder500: '#6D6D6D',
-  				boulder600: '#5D5D5D',
-  				boulder700: '#4F4F4F',
-  				boulder900: '#3D3D3D',
-  				boulder950: '#262626',
-  				madras: '#423306',
-  				activeLink: '#0F2942',
-  				inActiveLink: '#BDBDBD',
-  				success: '#039855'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      screens: {
+        "380": "380px",
+        "1186": "1186px",
+        "2xl": "1400px",
+        desktop: "1000px",
+        mobile: "500px",
+        tablet: {
+          raw: "(max-width: 999px)",
+        },
+        large: {
+          raw: "(min-width: 1440px) and (min-height: 1040px)",
+        },
+      },
+      maxWidth: {
+        desktop: "1200px",
+        createPage: "850px",
+      },
+      maxHeight: {
+        emptyBox: "250px",
+      },
+      width: {
+        "107": "107px",
+      },
+      height: {
+        "107": "107px",
+        allEventsSection: "calc(100% - 128px)",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: {
+          muted: "#f9fafb",
+          subtle: "#f3f4f6",
+          DEFAULT: "#ffffff",
+          emphasis: "#374151",
+          darkYellow: "#FFBB0A",
+          yellow700: "#EAB308",
+          brown: "#876307",
+          lightYellow: "#D5AE35",
+          white300: "#FEFEFE",
+          whisperGray: "#f8f7fa",
+          whisperWhite: "rgb(248, 247, 250)",
+          darkPurple: "rgba(30, 10, 60, 0.07)",
+        },
+        content: {
+          subtle: "#9ca3af",
+          DEFAULT: "#6b7280",
+          emphasis: "#374151",
+          strong: "#111827",
+          inverted: "#ffffff",
+          grayText: "#575757",
+        },
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+          boulder: "#7B7B7B",
+          boulder50: "#F6F6F6",
+          boulder100: "#E7E7E7",
+          tan: "#111111",
+          boulder200: "#D1D1D1",
+          boulder300: "#B0B0B0",
+          boulder400: "#7B7B7B",
+          boulder500: "#6D6D6D",
+          boulder600: "#5D5D5D",
+          boulder700: "#4F4F4F",
+          boulder900: "#3D3D3D",
+          boulder950: "#262626",
+          madras: "#423306",
+          activeLink: "#0F2942",
+          inActiveLink: "#BDBDBD",
+          success: "#039855",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
